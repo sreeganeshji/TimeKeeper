344d6ee0422771f4ae49aecb8a8eb04583f0e00c
Ganesh Bangalore <sree.ganeshji@gmail.com>
2023-08-12 12:01:27 -0400

    Initial Commit

---
+++ Time Keeper/Time_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_KeeperApp.swift
+//  Time Keeper
+//
+//  Created by Ganesh Bangalore on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Time_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
